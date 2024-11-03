ddf5b7c1ee6d4d7981a56060ea26d30848e4ee65
iritka-1 <ili417060@gmail.com>
2024-11-03 13:22:42 +0300

    Add files via upload

---
+++ Лабораторная работа 3/task1.py
@@ -0,0 +1,14 @@
+def func(fruits, item):
+    if item in fruits:
+        return fruits.index(item)
+    else:
+        return None
+
+items_list = ['яблоко', 'банан', 'апельсин', 'груша', 'киви', 'банан']
+
+for find_item in ['банан', 'груша', 'персик']:
+    index_item = func(items_list, find_item)  # TODO Вызовите функцию, что получить индекс товара
+    if index_item is not None:
+        print(f"Первое вхождение товара '{find_item}' имеет индекс {index_item}.")
+    else:
+        print(f"Товар '{find_item}' не найден в списке.")
